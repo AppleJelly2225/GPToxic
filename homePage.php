39a7318d5fc1e4f3af3d1c3b116565134cb168af
Audrianna Kelly <163651302+AppleJelly2225@users.noreply.github.com>
2025-01-23 20:24:17 -0500

    Home page

---
+++ homePage.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>GPToxic</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            
+
+        <style>
+			body,h1,h2,h3,h4,h5,p {font-family: "Raleway", sans-serif}
+			.col-sm-4 {background-color: #63666A; color: #E6EEF7;}
+		</style>
+		<!--ANIMATIONS! <^u*)-->
+		<style> 
+		</style>
+	</head>
+	<body style = "background-image: linear-gradient(to right, #448b33, #232920);">
+	
+	<div class = "jumbotron" style = "border-bottom: 2px solid black; height: 240px; color: #D6EF9F; background-image: url('flow.jpg'); background-size: cover; ">
+		<h1>GPToxic Live Site</h1>
+		<p>Presented by VMI Cadets--Roberts, Kelly, Poe</p>
+	</div>
+
+	<!-- <div class ="row" style= "margin-right: 5%; margin-left: 5%;"> <a href "http://localhost/precap/homePage.php" style="color: #D6EF9F; padding: 15px"> Home </a> &nbsp <a href "http://localhost/precap/aboutUs.php" style="color: #D6EF9F; padding: 15px"> About Us </a>  &nbsp <a href "http://localhost/precap/LessonIndex.php" style="color: #D6EF9F; padding: 15px"> Lessons </a> &nbsp <a href "*" style="color: #D6EF9F; padding: 15px"> Empty </a> &nbsp <a href "*" style="color: #D6EF9F; padding: 15px"> Empty </a> &nbsp <a href "*" style="color: #D6EF9F; padding: 15px"> Forum </a> &nbsp <a href "*" style="color: #D6EF9F; padding: 15px"> Help </a> </div><br> -->
+
+	<div class="container">
+
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="aboutUs-tab" data-bs-toggle="tab" data-bs-target="#aboutUs-tab-pane" type="button" role="tab" aria-controls="aboutUs-tab-pane" aria-selected="true">About Us</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="lessons-tab" data-bs-toggle="tab" data-bs-target="#lessons-tab-pane" type="button" role="tab" aria-controls="lessons-tab-pane" aria-selected="true">Lessons</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="lab-tab" data-bs-toggle="tab" data-bs-target="#lab-tab-pane" type="button" role="tab" aria-controls="lab-tab-pane" aria-selected="true">Labs & Quizzes</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="true">Contact</button>
+  </li>
+  <li class="nav-item" role="presentation">
+    <button class="nav-link" id="help-tab" data-bs-toggle="tab" data-bs-target="#help-tab-pane" type="button" role="tab" aria-controls="help-tab-pane" aria-selected="true">Help</button>
+  </li>
+  
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade" id="aboutUs-tab-pane" role="tabpanel" aria-labelledby="aboutUs-tab" tabindex="0" style="color: #D6EF9F"> <h1>About Us</h1> <p>Jude Roberts: Researcher, head software engineer, scrum master<br>
+        About Jude: urna purus, feugiat nec dui at, porta condimentum tellus. Etiam feugiatmauris at felis porttitor, at malesuada leo commodo. Sed tempor tortor erat, vitae euismod est aliquam non. Vivamus posuere sem sit amet interdum pretium. Sed vestibulum massa vel feugiat elementum. Mauris in tortor
+        vestibulum, malesuada purus eu, facilisis mauris. Sed lectus dolor, aliquet a aliquet id, sollicitudin sed ante. Cras sed mauris id enim ultrices ultrices ac quis dolor. Praesent ornare tristique massa scelerisque iaculis. 
+        Vestibulum ornare dui arcu, ut semper ex gravida et. Sed viverra erat ligula, eget placerat ante euismod sit amet. Donec commodo, tellus in convallis egestas, tellus diam volutpat dolor, et fringilla dolor erat eget nisi. 
+        Vivamus pellentesque leo nisl, sit amet tincidunt lectus ullamcorper quis.</p> <br> <p>Michael Poe: Document specialist & product owner <br> About Michael: Fusce ullamcorper viverra lectus nec posuere. Duis aliquet purus nec dignissim ultrices. Donec vitae suscipit lorem. Vestibulum cursus vel neque sit amet 
+        aliquam. Phasellus ac ligula eu felis aliquet efficitur. Proin porta, justo eget molestie dapibus, ante nisi dignissim lacus, ut euismod sapien ante in dui. Nam dictum aliquet consectetur. Aliquam sagittis 
+        porttitor turpis non ullamcorper. Sed pharetra, justo ut auctor cursus, risus nulla tincidunt sem, vitae mollis nibh ex iaculis eros.</p> <br> <p>Audrianna Kelly: UX/UI specialist & software engineer <br> About Audrianna: Vestibulum ornare dui arcu, ut semper ex gravida et. Sed viverra erat ligula, eget placerat 
+        ante euismod sit amet. Donec commodo, tellus in convallis egestas, tellus diam volutpat dolor, et fringilla dolor erat eget nisi. Vivamus pellentesque leo nisl, sit amet tincidunt lectus ullamcorper quis. Fusce ullamcorper viverra lectus nec posuere. Duis aliquet purus nec dignissim ultrices. Donec vitae
+        suscipit lorem. Vestibulum cursus vel neque sit amet aliquam. Phasellus ac ligula eu felis aliquet efficitur. Proin porta, justo eget molestie dapibus, ante nisi dignissim lacus, ut euismod sapien ante in dui. Nam dictum aliquet consectetur. Aliquam sagittis porttitor turpis non ullamcorper. Sed pharetra,
+        justo ut auctor cursus, risus nulla tincidunt sem, vitae mollis nibh ex iaculis eros. <br><br> Sherif Abdelhamid: Project advisor/supervisor <br> About Sherif: Curabitur ut luctus dui, vitae rutrum felis. Aliquam ut nisi vel enim ullamcorper mattis vitae id libero. Etiam ultrices leo bibendum ex ultrices, 
+        ut efficitur dolor sagittis. Sed urna purus, feugiat nec dui at, porta condimentum tellus. Etiam feugiat mauris at felis porttitor, at malesuada leo commodo. Sed tempor tortor erat, vitae euismod est aliquam non. Vivamus posuere sem sit amet interdum pretium. Sed vestibulum massa vel feugiat elementum. Mauris 
+        in tortor vestibulum, malesuada purus eu, facilisis mauris. Sed lectus dolor, aliquet a aliquet id, sollicitudin sed ante. Cras sed mauris id enim ultrices ultrices ac quis dolor. Praesent ornare tristique massa scelerisque iaculis.</p></div>
+  <div class="tab-pane fade" id="lessons-tab-pane" role="tabpanel" aria-labelledby="lessons-tab" tabindex="0" style="color: #D6EF9F"> 
+    <h1>Lessons</h1> <br> 
+    <form>
+      <button type: submit style="background-color: #D6EF9F; padding: 50px 75px; color: black; border-radius: 10px">Lesson 1: AI Poisoning</button>
+      <button type: submit style="background-color: #D6EF9F; padding: 50px 75px; color: black; border-radius: 10px">Lesson 2: AI in Resumes</button> 
+      <br><br>
+      <button type: submit style="background-color: #D6EF9F; padding: 50px 75px; color: black; border-radius: 10px;">Lesson 3: Oralce (??)</button>
+      <!-- <button type: submit style="background-color: indigo; padding: 50px 75px; color: white; border-radius: 8px">Lesson 4: idk</button> -->
+    </form>
+  </div> <!-- background-image: url('bias.jpg') -->
+  <div class="tab-pane fade" id="lab-tab-pane" role="tabpanel" aria-labelledby="lab-tab" tabindex="0" style="color: #D6EF9F"> 
+    <h1>Labs & Quizzes</h1>
+    <form>
+          <button type: submit word-wrap: break-word style="background-color: #D6EF9F; padding: 50px 75px; color: black; border-radius: 10px">Who's the robot?(AI patterns)</button>
+          <button type: submit style="background-color: #D6EF9F; padding: 50px 75px; color: black; border-radius: 10px">Dr. Injection (AI Injection Prompts)</button> 
+          <br><br>
+          <button type: submit style="background-color: #D6EF9F; padding: 50px 75px; color: black; border-radius: 10px;">Drawing with AI</button>
+        </form>
+  </div>
+  <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0" style="color: #D6EF9F"> <h1>Contacts</h1> <p>Jude Roberts: #123-456-789 <br><br> Michael Poe: #246-802-468
+<br><br> Audrianna Kelly: #098-765-4321 <br><br> Mr. Sherif Abdelhamid: #777-871-0211</p></div>
+  <div class="tab-pane fade" id="help-tab-pane" role="tabpanel" aria-labelledby="help-tab" tabindex="0" style="color: #D6EF9F"> <h1>FAQ</h1> </div>
+</div>
+	</div>
+	
+	</body>
+</html>
